ebc5b06d5263d8e0aedac30e5fbdffb866d7bc2f
Shivam Gupta <shivamrgupta17@gmail.com>
2022-11-09 14:35:21 +0530

    Updates

---
+++ precache-manifest.2ae610f5883b6a4f262014eddbb99a8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "67aaecc7d1f07e094ad6",
+    "url": "/My-App-React/static/css/main.b7a8b55b.chunk.css"
+  },
+  {
+    "revision": "67aaecc7d1f07e094ad6",
+    "url": "/My-App-React/static/js/main.67aaecc7.chunk.js"
+  },
+  {
+    "revision": "bfaef02fc73415496900",
+    "url": "/My-App-React/static/js/1.bfaef02f.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/My-App-React/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "b2727838399d669b4c5c",
+    "url": "/My-App-React/static/js/runtime~main.b2727838.js"
+  },
+  {
+    "revision": "01b3df53dab201169b165378c6685261",
+    "url": "/My-App-React/index.html"
+  }
+];
